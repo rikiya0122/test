b23b59ea13c4e0f57f1b0df186213f46b3b488a8
rikiya0122 <rikiya0122@gmail.com>
2019-01-30 13:56:04 +0900

    2回目の変更

--- Book.java
+++ Book.java
@@ -1,6 +1,7 @@
 public calss Book{
 	public void main(String[]args){
 		Sysytem out print("テスト１");
+		Sysytem out print("テスト2");
 		System out println(dip().toString());
 	}
 	public int disp(int a){
